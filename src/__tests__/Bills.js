789d31c1075f693f05621696e14f6a78f5a17d3f
RominM <rominmanogil.off@gmail.com>
2021-12-07 19:06:26 +0100

    localStorage is defined

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,8 +1,7 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { localStorageMock } from "../__mocks__/localStorage.js"
-import Firestore from "../app/Firestore";
+import { localStorageMock } from "../__mocks__/localStorage.js" // causee localStorage was not defined
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -10,23 +9,15 @@ describe("Given I am connected as an employee", () => {
       // const html = BillsUI({ data: []})
       // document.body.innerHTML = html
       //to-do write expect expression
-      Firestore.bills = () => ({ bills, get: jest.fn().mockResolvedValue() });
-      Object.defineProperty(window, "localStorage", { value: localStorageMock });
-      window.localStorage.setItem("user",JSON.stringify({
-          type: "Employee",
-        })
-      );
 
-      const html = BillsUI({ data: bills });
-      const pathname = ROUTES_PATH["Bills"];
-      Object.defineProperty(window, "location", { value: { hash: pathname } });
-      document.body.innerHTML = `<div id='root'>${html}</div>`;
-      Router();
+      // to call localStorage
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })// mock localStorage
+        window.localStorage.setItem('user', JSON.stringify({type: 'Employee'}))// Set user as Employee in localStorage
 
-      expect(screen.getByTestId("icon-window").classList.contains("active-icon")).toBe(true);
-    })
+        document.body.innerHTML = `<div id="root"></div>`;
+    });
     test("Then bills should be ordered from earliest to latest", () => {
-      const html = BillsUI({ data: bills })
+      const html = BillsUI({ data: bills }, {formatDate: false})
       document.body.innerHTML = html
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
