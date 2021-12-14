b5cbc6796f5465df8bc5c288bf00706cd15d5b16
RominM <rominmanogil.off@gmail.com>
2021-12-14 12:04:05 +0100

    writing the test for handle file format and error message and taping the code --green

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,136 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
+// In test the "{ ... }" is for the testing chaines
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { ROUTES } from "../constants/routes";
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
+  Object.defineProperty(window, 'localStorage', {
+    value: localStorageMock
+  })
+  window.localStorage.setItem('user', JSON.stringify({
+    type: 'Employee'
+  }))
+
+  // test function handleChangeFile -> Bon ou mouvais format de fichier
+  describe("When i change file", () => {
+    test("Then the function handleChangeFile is called", () => {
+      // context
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({
+          pathname
+        })
+      }
+      // create a NewBill
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage,
+      })
+      // get handleSubmit function
+      const handleChangeFile = jest.fn(newBill.handleChangeFile)
+      const file = screen.getByTestId("file")
+      file.addEventListener("change", handleChangeFile)
+      fireEvent.change(file, {
+        target: {
+          files: [new File(["text.txt"], "text.txt", {
+            type: "text/txt"
+          })],
+        }
+      })
+      expect(handleChangeFile).toBeCalled()
+      expect(document.querySelector("#errorMessagId").style.display).toBe("block");
+    })
+
+
+
+
+    // test message d'erreur
+    test("Then the error message = none", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
-      //to-do write assertion
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({
+          pathname
+        })
+      }
+      const firestore = {
+        storage: {
+          ref: jest.fn(() => {
+            return {
+              put: jest
+                .fn()
+                .mockResolvedValueOnce({
+                  ref: {
+                    getDownloadURL: jest.fn()
+                  }
+                }),
+            };
+          }),
+        },
+      };
+      // create a newBill
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore,
+        localStorage: window.localStorage,
+      })
+      // get handleChangeFile function
+      const handleChangeFile = jest.fn(newBill.handleChangeFile)
+      const file = screen.getByTestId("file")
+      file.addEventListener("change", handleChangeFile)
+      fireEvent.change(file, {
+        target: {
+          files: [new File(["image"], "image.jpg", {
+            type: "image/jpg"
+          })],
+        }
+      })
+      // test
+      expect(file.files.length).toEqual(1);
+      expect(document.querySelector("#errorMessagId").style.display).toBe("none");
     })
   })
-})
+
+  //test function handleSubmit
+  describe("When I'm on NewBill page and click on submit btn", () => {
+    test("Then the function handleSubmit should be called", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({
+          pathname
+        })
+      }
+      // create a NewBill
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage,
+      });
+
+      // get the form
+      const form = document.querySelector(`form[data-testid="form-new-bill"]`);
+      // get handleSubmit function
+      const handleSubmit = jest.fn(newBill.handleSubmit);
+      // listen the submit
+      form.addEventListener("submit", handleSubmit);
+      // simulated a btn type "submit"
+      fireEvent.submit(form);
+      // test
+      expect(handleSubmit).toHaveBeenCalled();
+    });
+  });
+});
--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -1,44 +1,76 @@
-
-import { ROUTES_PATH } from '../constants/routes.js'
+import {
+  ROUTES_PATH
+} from '../constants/routes.js'
 import Logout from "./Logout.js"
 
 export default class NewBill {
-  constructor({ document, onNavigate, firestore, localStorage }) {
+  constructor({
+    document,
+    onNavigate,
+    firestore,
+    localStorage
+  }) {
     this.document = document
     this.onNavigate = onNavigate
     this.firestore = firestore
+
     const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
     formNewBill.addEventListener("submit", this.handleSubmit)
     const file = this.document.querySelector(`input[data-testid="file"]`)
     file.addEventListener("change", this.handleChangeFile)
     this.fileUrl = null
     this.fileName = null
-    new Logout({ document, localStorage, onNavigate })
-  }
+    new Logout({
+      document,
+      localStorage,
+      onNavigate
+    })
+
+    //Création du msg d'erreur
+    const error = this.document.createElement('span');
+    error.innerHTML = "<strong>⚠</strong> Veuillez choisir un format png, jpg ou jpeg";
+    error.id = 'errorMessagId';
+    error.style.display = "none";
+
+    const label = document.querySelector(`div[data-testid="errorMessag"]`);
+    
+    label.appendChild(error);
+  };
+
   handleChangeFile = e => {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    this.firestore
-      .storage
-      .ref(`justificatifs/${fileName}`)
-      .put(file)
-      .then(snapshot => snapshot.ref.getDownloadURL())
-      .then(url => {
-        this.fileUrl = url
-        this.fileName = fileName
-      })
+    const fileName = file.name
+    const extension = fileName.split(".").pop()
+    let error = document.getElementById("errorMessagId")
+
+    // 3rd error [Bug Hunt] - Bills
+    if (extension === 'jpg' || extension === 'jpeg' || extension === 'png') {
+      this.firestore
+        .storage
+        .ref(`justificatifs/${file.name}`)
+        .put(file)
+        .then(snapshot => snapshot.ref.getDownloadURL())
+        .then(url => {
+          this.fileUrl = url
+          this.fileName = file.name
+        });
+      error.style.display = "none";
+    } else {
+      const test = this.document.querySelector(`input[data-testid="file"]`);
+      test.value = "";
+      error.style.display = "block";
+    }
   }
+
   handleSubmit = e => {
     e.preventDefault()
-    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
     const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
       amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
       pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
       commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
@@ -50,16 +82,16 @@ export default class NewBill {
     this.onNavigate(ROUTES_PATH['Bills'])
   }
 
-  // not need to cover this function by tests
+  /* istanbul ignore next */
   createBill = (bill) => {
     if (this.firestore) {
       this.firestore
-      .bills()
-      .add(bill)
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => error)
+        .bills()
+        .add(bill)
+        .then(() => {
+          this.onNavigate(ROUTES_PATH['Bills'])
+        })
+        .catch(error => error)
     }
   }
 }
